b59889ad9bf1a0d33b5a170ac4572d2caad60b26
Maynak Sannigrahi <maynak.medivirt@gmail.com>
2024-05-13 19:14:18 +0530

    Schedule meeting

--- src/components/DoctorSchedule.jsx
+++ src/components/DoctorSchedule.jsx
@@ -3,14 +3,23 @@ import { useParams } from "react-router-dom";
 import DoctorNavbar from './DoctorNavbar';
 import DoctorSide from './DoctorSide';
 import { getFirestore, collection, query, where, getDocs, doc, getDoc } from 'firebase/firestore';
+import Calendar from 'react-calendar'; // Import the Calendar component
+import Swal from 'sweetalert2'; // Import SweetAlert
 
 const DoctorSchedule = () => {
     const [scheduleMeetings, setScheduleMeetings] = useState([]);
     const [sidebarOpen, setSidebarOpen] = useState(true);
+    const [showCalendar, setShowCalendar] = useState(false); // State for calendar visibility
+    const [selectedDate, setSelectedDate] = useState(new Date()); // State for selected date
+    const [selectedTime, setSelectedTime] = useState(null); // State for selected time
     const { id } = useParams();
 
     const toggleSidebar = () => {
         setSidebarOpen(!sidebarOpen);
+    };
+
+    const toggleCalendar = () => {
+        setShowCalendar(!showCalendar);
     };
 
     useEffect(() => {
@@ -20,7 +29,6 @@ const DoctorSchedule = () => {
                 const scheduleMeetingsRef = collection(db, "scheduleMeeting");
                 const q = query(scheduleMeetingsRef, where("doctorID", "==", id));
                 const querySnapshot = await getDocs(q);
-                // const meetingsData = [];
 
                 const fetchCompanyData = async (companyId) => {
                     const companyDocRef = doc(db, "companies", companyId);
@@ -34,22 +42,14 @@ const DoctorSchedule = () => {
 
                 const promises = querySnapshot.docs.map(async (doc) => {
                     const meetingData = doc.data();
-                    // console.log(meetingData);
-                    
-                    // Fetch company data
                     const companyData = await fetchCompanyData(meetingData.companyID);
                     const companyName = companyData ? companyData.companyName : "Unknown Company";
                     const representativeName = companyData ? companyData.name : "Unknown Representative";
                 
-                    // Fetch doctor data
-                    // const doctorData = await fetchDoctorData(meetingData.doctorID);
-                    // const doctorName = doctorData ? doctorData.name : "Unknown Doctor";
-                    
                     return {
                         id: doc.id,
                         companyName,
                         representativeName,
-                        // doctorName,
                         ...meetingData,
                     };
                 });
@@ -63,7 +63,29 @@ const DoctorSchedule = () => {
 
         fetchScheduleMeetings();
     }, [id]);
-    
+
+    const handleModify = () => {
+        // Show confirmation dialog
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, save it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                // Perform save action here
+                toggleCalendar(); // Close the calendar after saving
+                // Delay the page refresh by 2 seconds
+                setTimeout(() => {
+                    window.location.reload();
+                }, 900);
+            }
+        });
+    };
+
     return (
         <div className="flex flex-col h-screen">
             <DoctorNavbar />
@@ -117,10 +139,17 @@ const DoctorSchedule = () => {
                                                 {meeting.time}
                                             </td>
                                             <td className="px-6 py-4 bg-gray-50">
-                                                <button type="button" className="text-white bg-[#7091E6] rounded-lg px-3 py-2 text-center me-2 mb-2">
+                                                <button
+                                                    onClick={toggleCalendar} // Open calendar on click
+                                                    className="text-white bg-[#7091E6] rounded-lg px-3 py-2 text-center me-2 mb-2"
+                                                >
                                                     Modify
                                                 </button>
-                                                <button type="button" className="text-white bg-[#7091E6] rounded-lg px-3 py-2 text-center me-2 mb-2">
+                                                <button
+                                                    // No onClick handler for the Accept button
+                                                    type="button"
+                                                    className="text-white bg-[#7091E6] rounded-lg px-3 py-2 text-center me-2 mb-2"
+                                                >
                                                     Accept
                                                 </button>
                                             </td>
@@ -132,6 +161,68 @@ const DoctorSchedule = () => {
                     </div>
                 </div>
             </div>
+            {showCalendar && (
+                <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center">
+                    <div className="bg-white p-6 rounded-lg">
+                        <div className="w-full max-w-xs">
+                            <Calendar
+                                onChange={setSelectedDate}
+                                value={selectedDate}
+                                className="border border-gray-300 rounded-md shadow-md"
+                                calendarClassName="bg-white p-4 rounded-lg shadow-lg"
+                                tileClassName={({ date, view }) =>
+                                    view === 'month' && date.getDay() === 0 ? 'bg-red-200' : null
+                                }
+                            />
+                        </div>
+                        <select
+                            value={selectedTime}
+                            onChange={(e) => setSelectedTime(e.target.value)}
+                            className="mt-3 block w-full px-2 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                        >
+                            <option value="">Select Time</option>
+                              <option value="09:00 AM">09:00 AM</option>
+                              <option value="09:30 AM">09:30 AM</option>
+                              <option value="10:00 AM">10:00 AM</option>
+                              <option value="10:30 AM">10:30 AM</option>
+                              <option value="11:00 AM">11:00 AM</option>
+                              <option value="11:30 AM">11:30 AM</option>
+                              <option value="12:00 PM">12:00 PM</option>
+                              <option value="12:30 PM">12:30 PM</option>
+                              <option value="1:00 PM">1:00 PM</option>
+                              <option value="1:30 PM">1:30 PM</option>
+                              <option value="2:00 PM">2:00 PM</option>
+                              <option value="2:30 PM">2:30 PM</option>
+                              <option value="3:00 PM">3:00 PM</option>
+                              <option value="3:30 PM">3:30 PM</option>
+                              <option value="4:00 PM">4:00 PM</option>
+                              <option value="4:30 PM">4:30 PM</option>
+                              <option value="5:00 PM">5:00 PM</option>
+                              <option value="5:30 PM">5:30 PM</option>
+                              <option value="6:00 PM">6:00 PM</option>
+                              <option value="6:30 PM">6:30 PM</option>
+                              <option value="7:00 PM">7:00 PM</option>
+                              <option value="7:30 PM">7:30 PM</option>
+                              <option value="8:00 PM">8:00 PM</option>
+                              <option value="8:30 PM">8:30 PM</option>
+                        </select>
+                        <div className="flex justify-end mt-4">
+                            <button
+                                onClick={toggleCalendar}
+                                className="px-4 py-2 mr-2 bg-gray-300 text-gray-800 rounded-md hover:bg-gray-400 focus:outline-none focus:bg-gray-400"
+                            >
+                                Cancel
+                            </button>
+                            <button
+                                onClick={handleModify}
+                                className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+                            >
+                                Save
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
